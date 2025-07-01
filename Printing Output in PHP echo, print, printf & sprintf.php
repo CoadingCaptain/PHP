62d37dccee83f76a0d5763c4cab251dc7d34e4b5
Parvez Shaikh <me.parvezj@gmail.com>
2025-07-01 23:23:04 +0600

    Add files via upload

---
+++ Printing Output in PHP echo, print, printf & sprintf.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World !".PHP_EOL ;
+
+// Output : Hello World !
+
+print "Welcome to PHP !".PHP_EOL ;
+
+// Output : Welcome to PHP !
+
+printf("Hello I am %s. I am from %s. I am %d years old.\n","Jack","London","20") ;
+
+// Output : Hello I am Jack. I am from London. I am 20 years old.
+
+$output = sprintf("Hello I am %s. I am from %s. I am %d years old.","Alex","Canada","25") ;
+echo $output ;
+
+// Output : Hello I am Alex. I am from Canada. I am 25 years old.
+
