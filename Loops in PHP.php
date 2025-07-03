f11c778752c2caefcb7eebbb9d66ad2db641fc50
Parvez Shaikh <me.parvezj@gmail.com>
2025-07-03 22:00:06 +0600

    Update Loops in PHP.php

--- Loops in PHP.php
+++ Loops in PHP.php
@@ -1,48 +1,48 @@
 <?php
 
-// ✅ Loop এর কাজ:
-// একই কোড বারবার চালানোর জন্য loop ব্যবহার করা হয় যতক্ষণ না একটি নির্দিষ্ট শর্ত পূরণ হয়।
+✅ Loop এর কাজ:
+ একই কোড বারবার চালানোর জন্য loop ব্যবহার করা হয় যতক্ষণ না একটি নির্দিষ্ট শর্ত পূরণ হয়।
 
-// 1. for loop
-// যখন জানা থাকে কতবার চলবে, তখন for loop ব্যবহার করা হয়।
+ 1. for loop
+ যখন জানা থাকে কতবার চলবে, তখন for loop ব্যবহার করা হয়।
 
-// for($i = 1 ; $i <= 5 ; $i++){
-//     echo $i ;
-// }
+ for($i = 1 ; $i <= 5 ; $i++){
+     echo $i ;
+ }
 
-// Output : 12345
-
-
-// 2. while loop
-// যতক্ষণ একটি শর্ত সত্য থাকে, ততক্ষণ কোড চলবে।
-
-// $i = 5 ;
-// while($i>0){
-//     echo $i ;
-//     $i-- ;
-// }
-
-// Output : 54321
+ Output : 12345
 
 
-// 3. do...while loop
-// কমপক্ষে একবার কোড চালাবে, তারপর শর্ত চেক করবে।
+ 2. while loop
+ যতক্ষণ একটি শর্ত সত্য থাকে, ততক্ষণ কোড চলবে।
 
-// $i = 1 ;
-// do{
-//     echo $i ;
-//     $i++ ;
-// }while($i<=5) ;
+ $i = 5 ;
+ while($i>0){
+     echo $i ;
+     $i-- ;
+ }
 
-// Output : 12345
+ Output : 54321
 
 
-// 4. foreach loop
-// array বা list এর উপরে লুপ চালানোর জন্য ব্যবহার হয়।
+ 3. do...while loop
+ কমপক্ষে একবার কোড চালাবে, তারপর শর্ত চেক করবে।
+
+ $i = 1 ;
+ do{
+     echo $i ;
+     $i++ ;
+ }while($i<=5) ;
+
+ Output : 12345
+
+
+ 4. foreach loop
+ array বা list এর উপরে লুপ চালানোর জন্য ব্যবহার হয়।
 
 $fruits = ['apple','orange','cherry','banana'] ;
 foreach($fruits as $item){
     echo $item." " ;
 }
 
-// Output : apple orange cherry banana 
+ Output : apple orange cherry banana 
