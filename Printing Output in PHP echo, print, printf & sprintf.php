1964d046837b666148c2a027d0a6d356a2791a80
Parvez Shaikh <me.parvezj@gmail.com>
2025-07-03 18:40:10 +0600

    Update Printing Output in PHP echo, print, printf & sprintf.php

--- Printing Output in PHP echo, print, printf & sprintf.php
+++ Printing Output in PHP echo, print, printf & sprintf.php
@@ -2,18 +2,17 @@
 
 echo "Hello World !" ;
 
-// Output : Hello World !
+ Output : Hello World !
 
 print "Welcome to PHP !" ;
 
-// Output : Welcome to PHP !
+ Output : Welcome to PHP !
 
 printf("Hello I am %s. I am from %s. I am %d years old.\n","Jack","London","20") ;
 
-// Output : Hello I am Jack. I am from London. I am 20 years old.
+ Output : Hello I am Jack. I am from London. I am 20 years old.
 
 $output = sprintf("Hello I am %s. I am from %s. I am %d years old.","Alex","Canada","25") ;
 echo $output ;
 
-// Output : Hello I am Alex. I am from Canada. I am 25 years old.
-
+ Output : Hello I am Alex. I am from Canada. I am 25 years old.
