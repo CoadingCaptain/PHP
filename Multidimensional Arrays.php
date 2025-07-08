0fa0c4d1602f9a314f525b7d8a85c28500c5ebf7
Parvez Shaikh <me.parvezj@gmail.com>
2025-07-08 16:50:45 +0600

    Update Multidimensional Arrays.php

--- Multidimensional Arrays.php
+++ Multidimensional Arrays.php
@@ -1,40 +1,40 @@
 <?php
 
-// ✅  Multidimensional Array মানে এমন একটি অ্যারে যার ভেতরে আরেকটি অ্যারে থাকে।
-// একটি অ্যারের ভেতরে এক বা একাধিক অ্যারে থাকে — এটিই Multidimensional Array।
+ Multidimensional Array মানে এমন একটি অ্যারে যার ভেতরে আরেকটি অ্যারে থাকে।
+ একটি অ্যারের ভেতরে এক বা একাধিক অ্যারে থাকে — এটিই Multidimensional Array।
 
-// $person = [
-//     ["name" => "Alex", "city" => "London"],
-//     ["name" => "Jack", "city" => "Canada"],
-// ];
-// print_r($person);
+ $person = [
+     ["name" => "Alex", "city" => "London"],
+     ["name" => "Jack", "city" => "Canada"],
+ ];
+ print_r($person);
 
-// Output : Array
-//          (
-//              [0] => Array
-//                  (
-//                      [name] => Alex
-//                      [city] => London
-//                  )
-//              [1] => Array
-//                  (
-//                      [name] => Jack
-//                      [city] => Canada
-//                  )
-//          )
+ Output : Array
+          (
+              [0] => Array
+                  (
+                      [name] => Alex
+                      [city] => London
+                  )
+              [1] => Array
+                  (
+                      [name] => Jack
+                      [city] => Canada
+                  )
+          )
 
 
-// Access nested elements
+✅ Access nested elements
 
-// $person = [
-//     ["name" => "Alex", "city" => "London"],
-//     ["name" => "Jack", "city" => "Canada"],
-// ];
-// print_r($person[0]["name"]);
+ $person = [
+     ["name" => "Alex", "city" => "London"],
+     ["name" => "Jack", "city" => "Canada"],
+ ];
+ print_r($person[0]["name"]);
 
-// Output : Alex
+ Output : Alex
 
-// Modify a nested value
+✅ Modify a nested value
 
 $person = [
     ["name" => "Alex", "city" => "London"],
@@ -43,16 +43,16 @@ $person = [
 $person[1]["name"] = "Doe";
 print_r($person);
 
-// Output : Array
-//          (
-//              [0] => Array
-//                  (
-//                      [name] => Alex
-//                      [city] => London
-//                  )
-//              [1] => Array
-//                  (
-//                      [name] => Doe
-//                      [city] => Canada
-//                  )
-//          )
+ Output : Array
+          (
+              [0] => Array
+                  (
+                      [name] => Alex
+                      [city] => London
+                  )
+              [1] => Array
+                  (
+                      [name] => Doe
+                      [city] => Canada
+                  )
+          )
