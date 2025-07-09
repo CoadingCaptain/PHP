77f0f105a2283830cafc653f0295bd942563bdec
Parvez Shaikh <me.parvezj@gmail.com>
2025-07-09 21:40:03 +0600

    Update Reading, Writing, and Deleting Files.php

--- Reading, Writing, and Deleting Files.php
+++ Reading, Writing, and Deleting Files.php
@@ -1,38 +1,38 @@
 <?php
 
-// ✅ Write to a file
+✅ Write to a file
 
-// file_put_contents("panda.text", "Hello, This is pandas secrete file.");
+ file_put_contents("panda.text", "Hello, This is pandas secrete file.");
 
-// ✅ Read from a file
+✅ Read from a file
 
-// echo file_get_contents("panda.text");
+ echo file_get_contents("panda.text");
 
-// Output : Hello, This is pandas secrete file.
+ Output : Hello, This is pandas secrete file.
 
-// ✅ Delete a file
+ ✅ Delete a file
 
-// unlink("panda.text");
+ unlink("panda.text");
 
 
-// 1. fwrite
+ ✅ 1. fwrite
 
-// $file = fopen("panda.text", "w");
-// fwrite($file, "Hello, this is pandas secrete file.");
-// fclose($file);
+ $file = fopen("panda.text", "w");
+ fwrite($file, "Hello, this is pandas secrete file.");
+ fclose($file);
 
-// 2. fread
+ ✅ 2. fread
 
-// $file = fopen("panda.text", "r");
-// echo fread($file, filesize("panda.text"));
-// fclose($file);
+ $file = fopen("panda.text", "r");
+ echo fread($file, filesize("panda.text"));
+ fclose($file);
 
-// Output : Hello, this is pandas secrete file.
+ Output : Hello, this is pandas secrete file.
 
-// Append Mood
+ ✅ 3. Append Mood
 
-// $file = fopen("panda.text", "a");
-// fwrite($file, "Password is 12345.");
-// fclose($file);
+ $file = fopen("panda.text", "a");
+ fwrite($file, "Password is 12345.");
+ fclose($file);
 
-// Output : Hello, this is pandas secrete file.Password is 12345.
+ Output : Hello, this is pandas secrete file.Password is 12345.
