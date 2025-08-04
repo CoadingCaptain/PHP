10402b419c87622d2c0181c5197760ac8b2eeef1
Parvez Shaikh <me.parvezj@gmail.com>
2025-08-05 00:04:50 +0600

    Update Loops in PHP.php

--- Loops in PHP.php
+++ Loops in PHP.php
@@ -106,4 +106,47 @@ $students = [
 foreach ($students[0] as $pKey => $pValue) {
     print_r($pKey . " : " . $pValue . " ") ;
 }
-Output : name : Raj age : 18 city : Dhaka 
+
+  Output : name : Raj age : 18 city : Dhaka 
+
+✅ Fibonacci Series
+
+$vo = 0;
+$o = 1;
+$n = 1;
+for ($i = 0; $i <= 10; $i++) {
+     echo $vo . "  ";
+     $o = $n;
+     $n = $vo + $o;
+     $vo = $o;
+}
+
+ Output : 0  1  1  2  3  5  8  13  21  34  55 
+
+✅ Factorial
+
+function factorial($n)
+{
+    $result = 1;
+    for ($i = $n; $i > 1; $i--) {
+        $result *= $i;
+    }
+    return $result;
+}
+echo factorial(5);
+
+ Output : 120
+
+✅ Sum using Variadic function 
+
+function sum(...$numbers)
+{
+    $result = 0;
+    for ($i = 0; $i < count($numbers); $i++) {
+        $result += $numbers[$i];
+    }
+    return $result;
+}
+print_r(sum(10, 20, 30, 40, 50, 60));
+
+ Output : 210
