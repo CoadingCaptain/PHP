b8a3c99f2f01d7fe0fc10621846f0ecbe9b0433d
Parvez Shaikh <me.parvezj@gmail.com>
2025-08-09 01:07:12 +0600

    Update String Manipulation.php

--- String Manipulation.php
+++ String Manipulation.php
@@ -2,176 +2,176 @@
 
 declare(strict_types=1);
 
-// ✅ String- কে ছোট থেকে বড় অক্ষরে রুপান্তর করতে
+✅ String- কে ছোট থেকে বড় অক্ষরে রুপান্তর করতে
 
-// $name = strtoupper("Bangladesh");
-// echo $name;
+$name = strtoupper("Bangladesh");
+echo $name;
 
-// Output : BANGLADESH
+ Output : BANGLADESH
 
-// ✅ String- কে বড় থেকে ছোট অক্ষরে রুপান্তর করতে
+✅ String- কে বড় থেকে ছোট অক্ষরে রুপান্তর করতে
 
-// $name = strtolower("BANGLADESH");
-// echo $name;
+$name = strtolower("BANGLADESH");
+echo $name;
 
-// Output : bangladesh
+ Output : bangladesh
 
-// ✅ String - এর প্রথম অক্ষর বড় করতে
+✅ String - এর প্রথম অক্ষর বড় করতে
 
-// $name = ucwords("bangladesh");
-// echo $name;
+$name = ucwords("bangladesh");
+echo $name;
 
-// Output : Bangladesh
+ Output : Bangladesh
 
-// ✅ String - এর লেন্থ বের করতে
+✅ String - এর লেন্থ বের করতে
 
-// $strLen = strlen("Bangladesh");
-// echo $strLen;
+$strLen = strlen("Bangladesh");
+echo $strLen;
 
-// Output : 10
+ Output : 10
 
-// ✅ String - কে concat করা বা জুড়ে দেওয়া
+✅ String - কে concat করা বা জুড়ে দেওয়া
 
-// $s1 = "Hello";
-// $s2 = "World";
-// echo $s1 . " " . $s2;
+$s1 = "Hello";
+$s2 = "World";
+echo $s1 . " " . $s2;
 
-// Output : Hello World
+ Output : Hello World
 
-// ✅ substr() - যেটা দিয়ে কোনো স্ট্রিং-এর নির্দিষ্ট অংশ (substring) কেটে নেয়া যায় ।
+✅ substr() - যেটা দিয়ে কোনো স্ট্রিং-এর নির্দিষ্ট অংশ (substring) কেটে নেয়া যায় ।
 
-// $name = substr("Bangladesh", 0, 6);
-// echo $name;
+$name = substr("Bangladesh", 0, 6);
+echo $name;
 
-// Output : Bangla
+ Output : Bangla
 
-// ✅ String - কে reverse করা বা উল্টে দেওয়া
+✅ String - কে reverse করা বা উল্টে দেওয়া
 
-// $name = strrev("Bangladesh");
-// echo $name;
+$name = strrev("Bangladesh");
+echo $name;
 
-// Output : hsedalgnaB
+ Output : hsedalgnaB
 
-// ✅ Another Way
+✅ Another Way
 
-// $name = "Bangladesh";
-// $rev = "";
-// for ($i = strlen($name) - 1; $i >= 0; $i--) {
-//     $rev .= $name[$i];
-// }
-// echo $rev;
+$name = "Bangladesh";
+$rev = "";
+for ($i = strlen($name) - 1; $i >= 0; $i--) {
+    $rev .= $name[$i];
+}
+echo $rev;
 
-// Output : hsedalgnaB
+ Output : hsedalgnaB
 
-// // ✅ str_repeat() -  যেটা দিয়ে একটি স্ট্রিংকে একাধিকবার রিপিট (পুনরাবৃত্তি) করা যায়।
+✅ str_repeat() -  যেটা দিয়ে একটি স্ট্রিংকে একাধিকবার রিপিট (পুনরাবৃত্তি) করা যায়।
 
-// $str = str_repeat("** ", 10);
-// echo $str;
+$str = str_repeat("** ", 10);
+echo $str;
 
-// Output : ** ** ** ** ** ** ** ** ** ** 
+ Output : ** ** ** ** ** ** ** ** ** ** 
 
-// ✅ Another Way
+✅ Another Way
 
-// $str = "😎 ";
-// $repeat = "";
-// for ($i = 1; $i <= 10; $i++) {
-//     $repeat .= $str;
-// }
-// echo $repeat;
+$str = "😎 ";
+$repeat = "";
+for ($i = 1; $i <= 10; $i++) {
+    $repeat .= $str;
+}
+echo $repeat;
 
-// Output : 😎 😎 😎 😎 😎 😎 😎 😎 😎 😎
+ Output : 😎 😎 😎 😎 😎 😎 😎 😎 😎 😎
 
-// str_shuffle() - যেটা দিয়ে একটি স্ট্রিং-এর অক্ষরগুলোর অর্ডার এলোমেলোভাবে (randomly) সাজানো হয়।
+str_shuffle() - যেটা দিয়ে একটি স্ট্রিং-এর অক্ষরগুলোর অর্ডার এলোমেলোভাবে (randomly) সাজানো হয়।
 
-// $str = str_shuffle("Bangladesh");
-// echo $str;
+$str = str_shuffle("Bangladesh");
+echo $str;
 
-// Output : adBgnealhs
+ Output : adBgnealhs
 
-// ✅ trim() - একটি স্ট্রিং-এর শুরু এবং শেষের অপ্রয়োজনীয় স্পেস (whitespace) বা নির্দিষ্ট কিছু ক্যারেক্টার কেটে ফেলা।
+✅ trim() - একটি স্ট্রিং-এর শুরু এবং শেষের অপ্রয়োজনীয় স্পেস (whitespace) বা নির্দিষ্ট কিছু ক্যারেক্টার কেটে ফেলা।
 
-// $str = trim("   Hello World   ");
-// echo $str;
+$str = trim("   Hello World   ");
+echo $str;
 
-// Output : Hello World
+ Output : Hello World
 
-// ✅ ltrim() - একটি স্ট্রিং-এর বাম দিকের অপ্রয়োজনীয় স্পেস (whitespace) কেটে ফেলে।
+✅ ltrim() - একটি স্ট্রিং-এর বাম দিকের অপ্রয়োজনীয় স্পেস (whitespace) কেটে ফেলে।
 
-// $str = ltrim("       Canada");
-// echo $str;
+$str = ltrim("       Canada");
+echo $str;
 
-// Output : Canada
+ Output : Canada
 
-// ✅ rtrim() - একটি স্ট্রিং-এর ডান দিকের অপ্রয়োজনীয় স্পেস (whitespace) কেটে ফেলে।
+✅ rtrim() - একটি স্ট্রিং-এর ডান দিকের অপ্রয়োজনীয় স্পেস (whitespace) কেটে ফেলে।
 
-// $str = rtrim(" Canada   ");
-// echo $str;
+$str = rtrim(" Canada   ");
+echo $str;
 
-// Output :  Canada
+ Output :  Canada
 
-// ✅ explode() - String to Array convert
+✅ explode() - String to Array convert
 
-// $str = explode(" ", "hello world");
-// print_r($str);
+$str = explode(" ", "hello world");
+print_r($str);
 
-// Output : Array
-//          (
-//              [0] => hello
-//              [1] => world
-//          )
+ Output : Array
+          (
+              [0] => hello
+              [1] => world
+          )
 
-// ✅ implode/join() - Array to string convert
+✅ implode/join() - Array to string convert
 
-// $words = ["Hello", "from", "Ostad", "team"];
-// $newWord = implode(" ", $words);
-// echo $newWord . ".";
+$words = ["Hello", "from", "Ostad", "team"];
+$newWord = implode(" ", $words);
+echo $newWord . ".";
 
-// Output : Hello from Ostad team.
+ Output : Hello from Ostad team.
 
-// Working
+Working
 
-// $name1 = "Md.  Abdullah";
-// $name2 = "Md..  Abdur Rahman";
-// $name3 = "Md  Abdul ..Malek";
-// $rep1 = str_replace("  ", " ", $name1);
-// $rep2 = str_replace(["  ", ".."], [" ", "."], $name2);
+$name1 = "Md.  Abdullah";
+$name2 = "Md..  Abdur Rahman";
+$name3 = "Md  Abdul ..Malek";
+$rep1 = str_replace("  ", " ", $name1);
+$rep2 = str_replace(["  ", ".."], [" ", "."], $name2);
 
-// echo $rep1;     Output : Md. Abdullah
-// echo $rep2;     Output: Md. Abdur Rahman
+echo $rep1;     Output : Md. Abdullah
+echo $rep2;     Output: Md. Abdur Rahman
 
-// ✅ str_split() -  যেটা কোনো স্ট্রিং (string) কে একটি অক্ষরভিত্তিক অ্যারে (array) তে ভেঙে ফেলে।
+✅ str_split() -  যেটা কোনো স্ট্রিং (string) কে একটি অক্ষরভিত্তিক অ্যারে (array) তে ভেঙে ফেলে।
 
 $str = str_split("Sweet");
 
-// Output : Array
-//          (
-//              [0] => S
-//              [1] => w
-//              [2] => e
-//              [3] => e
-//              [4] => t
-//          )
+ Output : Array
+          (
+              [0] => S
+              [1] => w
+              [2] => e
+              [3] => e
+              [4] => t
+          )
 
-// $str = str_split("Beauty", 2);
-// print_r($str);
+$str = str_split("Beauty", 2);
+print_r($str);
 
-// Output : Array
-//          (
-//              [0] => Be
-//              [1] => au
-//              [2] => ty
-//          )
+ Output : Array
+          (
+              [0] => Be
+              [1] => au
+              [2] => ty
+          )
 
-// ✅ strpos() - এর কাজ হলো একটি string-এর ভিতরে কোনো নির্দিষ্ট শব্দ বা অক্ষর কোথায় আছে তা খুঁজে বের করা। এটি প্রথম মিলে যাওয়ার অবস্থান (index) রিটার্ন করে।
+✅ strpos() - এর কাজ হলো একটি string-এর ভিতরে কোনো নির্দিষ্ট শব্দ বা অক্ষর কোথায় আছে তা খুঁজে বের করা। এটি প্রথম মিলে যাওয়ার অবস্থান (index) রিটার্ন করে।
 
-// $report = "Rahim,Karim,Asif,Motin aj k school theke palaise";
-// $search = "Monika";
-// $find = strpos($report, $search);
+$report = "Rahim,Karim,Asif,Motin aj k school theke palaise";
+$search = "Monika";
+$find = strpos($report, $search);
 
-// if ($find !== false) {
-//     echo "{$search} aj k school theke palaise 😂";
-// } else {
-//     echo "{$search} aj k school theke palayny 😊";
-// }
+if ($find !== false) {
+    echo "{$search} aj k school theke palaise 😂";
+} else {
+    echo "{$search} aj k school theke palayny 😊";
+}
 
-// Output : Monika aj k school theke palayny 😊
+ Output : Monika aj k school theke palayny 😊
